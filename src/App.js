8d66bffd7e5bbc93fe52270e6acf58a2db640643
Antoine Harel <antoine.harel@epita.fr>
2018-11-19 17:48:00 +0100

    RATP Status

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import MyMenu from './components/menu'
+import './style/main.css'
+import {Grid, Message, Icon, Container} from 'semantic-ui-react'
 
-var Home = () => {return <p>Home</p>};
-var About = () => {return <p>About</p>};
-
-class NoMatch extends Component {
-    render()
-    {
-        return (<p>{this.props.match.params.url}</p>)
+class Lines extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+        this.props.metros.map((line) => <Grid.Column>
+            <div className="line-status">
+                <img className="line-logo " src={"./img/lignes/" + line.line + ".svg"}/>
+                <br />
+                {
+                    line.slug === "normal"
+                    && <Icon name="circle" className="status-indicator"/>
+                    || <Icon name="circle" className="status-indicator red"/>
+                }
+            </div>
+        </Grid.Column>)
+    )
     }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="./img/logo.jpg" className="App-logo" alt="logo" />
-          <Router>
-              <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/:url" component={NoMatch} />
-              </Switch>
-          </Router>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            "loaded": false
+        }
+    }
+    componentDidMount() {
+        fetch('https://api-ratp.pierre-grimaud.fr/v3/traffic/metros').then(response => response.json()).then(data => this.setState({"metros": data.result.metros, "loaded": true}));
+    }
+    render() {
+        if (this.state.loaded)
+        {
+        return (<div>
+            <MyMenu/>
+            <div className="content">
+                <Container>
+                    <Message>
+                        <Message.Header>
+                            Metros
+                        </Message.Header>
+                        <Grid centered stackable="stackable" columns={16}>
+                            <Lines metros={this.state.metros} />
+                        </Grid>
+                    </Message>
+                </Container>
+            </div>
+        </div>);
+        }
+        return <h1>Loading</h1>
+    }
 }
 
 export default App;
---
+++ src/components/menu.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Menu } from 'semantic-ui-react'
+
+class MyMenu extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu stackable inverted>
+        <Menu.Item>
+          <img src='./img/ratp-notext.png' />
+        </Menu.Item>
+
+
+
+      </Menu>
+    )
+  }
+}
+
+export default MyMenu;
