f818655568f4e4faf857a116dde5a63088a9dbd2
Antoine Harel <antoine.harel@epita.fr>
2018-11-19 11:37:29 +0100

    Added basic router + logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+var Home = () => {return <p>Home</p>};
+var About = () => {return <p>About</p>};
+var NoMatch = () => {return <p>404</p>};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src="./img/logo.jpg" className="App-logo" alt="logo" />
+          <Router>
+              <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route component={NoMatch} />
+              </Switch>
+          </Router>
         </header>
       </div>
     );
