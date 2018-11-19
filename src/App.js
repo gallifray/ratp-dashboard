8b2192be77b5711fffd74d68ca9880e206424a41
Antoine Harel <antoine.harel@epita.fr>
2018-11-19 21:09:40 +0100

    Added tramways & rers

--- src/App.js
+++ src/App.js
@@ -5,30 +5,98 @@ import MyFooter from './components/footer'
 import './style/main.css'
 import { Popup, Grid, Message, Icon, Container } from 'semantic-ui-react'
 
-class Lines extends Component {
+class LinesTramway extends Component {
     constructor(props) {
         super(props);
     }
     render() {
         return(
-        this.props.metros.map((line) =>
+        this.props.data.map((line) =>
         <Popup
           position='bottom center'
           size='tiny'
           key={line.line}
-          trigger={        <Grid.Column>
-                      <div className="line-status">
-                          <img className="line-logo " src={"./img/lignes/" + line.line + ".svg"}/>
-                          <br />
+          trigger={
+              <Grid.Column>
+                  <div className="line-status">
+                      <img className="line-logo " src={"./img/lignes/tramway/" + line.line + ".svg"}/>
+                      <br />
+                      {
+                          line.slug === "normal"
+                          && <Icon name="circle" className="status-indicator"/>
+                          || (line.slug === "normal_trav"
+                              && <div><Icon name="circle" className="status-indicator"/>
+                          <Icon name="warning sign" className="status-indicator orange"/></div>
+                              || <Icon name="circle" className="status-indicator red"/>)
+                      }
+                  </div>
+              </Grid.Column>}
+          header={line.title}
+          content={line.message}
+        />)
+    )
+    }
+}
+
+class LinesMetro extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+        this.props.data.map((line) =>
+        <Popup
+          position='bottom center'
+          size='tiny'
+          key={line.line}
+          trigger={
+              <Grid.Column>
+                  <div className="line-status">
+                      <img className="line-logo " src={"./img/lignes/metro/" + line.line + ".svg"}/>
+                      <br />
                           {
                               line.slug === "normal"
                               && <Icon name="circle" className="status-indicator"/>
                               || (line.slug === "normal_trav"
-                                  && <Icon name="warning sign" className="status-indicator orange"/>
+                                  && <div><Icon name="circle" className="status-indicator"/>
+                              <Icon name="warning sign" className="status-indicator orange"/></div>
                                   || <Icon name="circle" className="status-indicator red"/>)
                           }
-                      </div>
-                  </Grid.Column>}
+                  </div>
+              </Grid.Column>}
+          header={line.title}
+          content={line.message}
+        />)
+    )
+    }
+}
+
+class LinesRer extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+        this.props.data.map((line) =>
+        <Popup
+          position='bottom center'
+          size='tiny'
+          key={line.line}
+          trigger={
+              <Grid.Column>
+                  <div className="line-status">
+                      <img className="line-logo " src={"./img/lignes/rer/" + line.line + ".svg"}/>
+                      <br />
+                          {
+                              line.slug === "normal"
+                              && <Icon name="circle" className="status-indicator"/>
+                              || (line.slug === "normal_trav"
+                                  && <div><Icon name="circle" className="status-indicator"/>
+                              <Icon name="warning sign" className="status-indicator orange"/></div>
+                                  || <Icon name="circle" className="status-indicator red"/>)
+                          }
+                  </div>
+              </Grid.Column>}
           header={line.title}
           content={line.message}
         />)
@@ -44,7 +112,14 @@ class App extends Component {
         }
     }
     componentDidMount() {
-        fetch('https://api-ratp.pierre-grimaud.fr/v3/traffic/metros').then(response => response.json()).then(data => this.setState({"metros": data.result.metros, "loaded": true}));
+        fetch('https://api-ratp.pierre-grimaud.fr/v3/traffic?_format=json')
+        .then(response => response.json())
+        .then(data => this.setState({
+            "metros": data.result.metros,
+            "rers": data.result.rers,
+            "tramways": data.result.tramways,
+            "loaded": true
+        }))
     }
     render() {
         if (this.state.loaded)
@@ -55,10 +130,30 @@ class App extends Component {
                 <Container>
                     <Message>
                         <Message.Header>
-                            Métros
+                            Métro
                         </Message.Header>
                         <Grid centered stackable="stackable" columns={16}>
-                            <Lines metros={this.state.metros} />
+                            <LinesMetro data={this.state.metros} />
+                        </Grid>
+                    </Message>
+                </Container>
+                <Container>
+                    <Message>
+                        <Message.Header>
+                            RER
+                        </Message.Header>
+                        <Grid centered stackable="stackable" columns={16}>
+                            <LinesRer data={this.state.rers} />
+                        </Grid>
+                    </Message>
+                </Container>
+                <Container>
+                    <Message>
+                        <Message.Header>
+                            Trawmay
+                        </Message.Header>
+                        <Grid centered stackable="stackable" columns={16}>
+                            <LinesTramway data={this.state.tramways} />
                         </Grid>
                     </Message>
                 </Container>
